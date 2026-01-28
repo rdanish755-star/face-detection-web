1f61ee00256af6b8cb4f86248b9d2371498b36b3
rdanish755-star <rdanish755@gmail.com>
2026-01-28 12:23:22 -0800

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ xml_path = os.path.join(base_path, 'haarcascade_frontalface_default.xml')
 face_cascade = cv2.CascadeClassifier(xml_path)
 
 def transform(frame):
+    st.write(f"Total Faces Detected:{len(faces)"}
     img = frame.to_ndarray(format="bgr24")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 3)
     
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
@@ -23,6 +24,7 @@ def transform(frame):
 
 
 webrtc_streamer(key="example", video_frame_callback=transform)
+
 
 
 
