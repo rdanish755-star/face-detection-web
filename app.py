7dfeed2583a9af65dd6ab9411d0fc44a29470dd9
rdanish755-star <rdanish755@gmail.com>
2026-01-28 12:26:57 -0800

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,17 @@ xml_path = os.path.join(base_path, 'haarcascade_frontalface_default.xml')
 face_cascade = cv2.CascadeClassifier(xml_path)
 
 def transform(frame):
-    st.write(f"Total Faces Detected:{len(faces)"})
     img = frame.to_ndarray(format="bgr24")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 3)
     
+    # Face detect karna
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    # Screen par count likhna (Video ke andar hi)
+    count_text = f"Faces: {len(faces)}"
+    cv2.putText(img, count_text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    
+    # Chehre par box banana
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
         
@@ -24,6 +30,7 @@ def transform(frame):
 
 
 webrtc_streamer(key="example", video_frame_callback=transform)
+
 
 
 
